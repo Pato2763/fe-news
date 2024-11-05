5986ab15c9ebcc3c7c356e8317c2b9fbe169e9c9
Pato2763 <daviespatrick2763@gmail.com>
2024-11-05 09:06:44 +0000

    added the comments to the individual article screen

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
-import { getArticleById } from "../api/api";
+import { getArticleById, getCommentsByArticleId } from "../api/api";
 import { useParams } from "react-router-dom";
 import { dateFormatter } from "../utils";
+import CommentCard from "./CommentCard";
 
 const Article = () => {
   const [article, setArticle] = useState("");
+  const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { article_id } = useParams();
 
   useEffect(() => {
-    getArticleById(article_id).then((articleData) => {
-      setArticle(articleData);
-      setIsLoading(false);
-    });
+    getArticleById(article_id)
+      .then((articleData) => {
+        setArticle(articleData);
+        return getCommentsByArticleId(article_id);
+      })
+      .then((commentsData) => {
+        setComments(commentsData);
+        setIsLoading(false);
+      });
   }, [article_id]);
 
   return (
@@ -33,6 +40,16 @@ const Article = () => {
           <p className="article-votes">Votes: {article.votes}</p>
         </section>
       )}
+      <section className="comments">
+        {comments.map((commentElement) => {
+          return (
+            <CommentCard
+              key={commentElement.comment_id}
+              comment={commentElement}
+            />
+          );
+        })}
+      </section>
     </main>
   );
 };
